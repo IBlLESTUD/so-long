40147c13c738b06d4d6c1d1ca4e4e4123356af64
Nathan Glaizaud <nglaizau@c1r5p3.42nice.fr>
2025-03-22 14:02:58 +0100

    push iheb

--- mlx.c
+++ mlx.c
@@ -6,7 +6,7 @@
 /*   By: nglaizau <nglaizau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 09:02:42 by nglaizau          #+#    #+#             */
-/*   Updated: 2025/03/06 17:18:39 by nglaizau         ###   ########.fr       */
+/*   Updated: 2025/03/17 11:57:35 by nglaizau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,74 @@ void	mlx_window(t_game *game, char **map)
  	game->mlx = mlx_init();
 	if (!game->mlx)
 		return (exit_error("failure"));
-	gameNathan Glaizaud
-ight = 0;
+	game->map = map;
+	game->height = 0;
 	while (map[game->height])
 		game->height++;
 	game->width = ft_strlen(map[0]);
 	game->window = mlx_new_window(game->mlx, game->width * 64,
 								game->height * 64, "so_long");
+	mlx_key_hook(game->window, handle_keypress, game);
+	mlx_hook(game->window, 17, 0, exit_game, game);
+	
 }
-int	handle_keypress(int keycode, )
+int	handle_keypress(int keycode, t_game *game)
 {
 	if (keycode == ESC_KEY)
-		exit(0);
+		exit_game(game, 0);
+	else if (keycode == W_KEY && game->player_y > 0)
+		moove(game, game->player_x, game->player_y -1);
+	else if (keycode == A_KEY && game->player_x > 0)
+		moove(game, game->player_y, game->player_x -1);
+	else if (keycode == S_KEY && game->player_y < game->height)
+		moove(game, game->player_x, game->player_y + 1);
+	else if (keycode == D_KEY && game->player_x < game->width)
+		moove(game, game->player_y, game->player_x + 1);
+	return (0);
+}
+void	moove(t_game *g, int new_x, int new_y)
+{
+	if (new_x < 0 || new_x >= g->width || new_y vois< 0 || new_y > g->height)
+		return ;
+	if (g->map[new_y][new_x] == '1')
+		return ;
+	ft_printf("moove player : %d\n", g->count);
+	if (g->map[new_y][new_x] == 'C')
+	{
+		g->map[new_y][new_x] = '0';
+		g->collected++;
+	}
+	if (g->map[new_y][new_x] == 'E')
+	{
+		if (g->collected == g->total_collected)
+		{
+			ft_printf("YOU WIN");
+			exit_game(g, 0);
+		}
+		return ;
+	}
+	g->map[g->player_y][g->player_x] = '0';
+	g->map[new_y][new_x] = 'P';
+	g->player_y = new_y;
+	g->player_x = new_x;
+	g->count++;
+}
+int exit_game(t_game *game, int flag)
+{
+	ft_printf("ggg");
+	// if (game->floor)
+	// 	mlx_destroy_image(game->mlx, game->floor);
+	// if (game->wall)
+	// 	mlx_destroy_image(game->mlx, game->wall);
+	// if (game->player)
+	// 	mlx_destroy_image(game->mlx, game->player);
+	// if (game->col)
+	// 	mlx_destroy_image(game->mlx, game->col);
+	// if (game->exit)
+	// 	mlx_destroy_image(game->mlx, game->exit);
+	mlx_destroy_window(game->mlx, game->window);
+	mlx_destroy_display(game->mlx);
+	free(game->mlx);
+	exit(flag);
 	return (0);
 }
--- so_long.h
+++ so_long.h
@@ -6,13 +6,17 @@
 /*   By: nglaizau <nglaizau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 23:09:33 by nglaizau          #+#    #+#             */
-/*   Updated: 2025/03/06 16:30:20 by nglaizau         ###   ########.fr       */
+/*   Updated: 2025/03/17 11:09:01 by nglaizau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 # define ESC_KEY 65307
+# define W_KEY 119
+# define A_KEY 97
+# define S_KEY 115
+# define D_KEY 100
 
 # include <unistd.h>
 # include <stdio.h>
@@ -39,6 +43,18 @@ typedef struct s_game
 	void	*mlx;
 	char	**map;
 	void	*window;
+	int		player_x;
+	int		player_y;
+	int		new_y;
+	int		nex_x;
+	int		count;
+	int		collected;
+	int		total_collected;
+	void	*floor;
+	void	*wall;
+	void	*player;
+	void	*col;
+	void	*exit;
 }	t_game;
 void		exit_error(char *str);
 void		check_fd(t_map *map, char *filename);
@@ -54,7 +70,9 @@ void		temp_map(t_map *cpy, char *filname);
 void		flood_fil(t_map *cpy, int i, int j);
 void		check_flood(t_map *cpy);
 void		mlx_window(t_game *game, char **map);
-//int			handle_keypress(int keycode);
+int			handle_keypress(int keycode, t_game *game);
 //void		all_collectible(t_map *cpy);
+void		moove(t_game *g, int new_x, int new_y);
+int			exit_game(t_game *game, int flag);
 
 #endif
